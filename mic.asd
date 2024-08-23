6cf87e79098b30f5cefe1c61ed5a72fa4c7058aa
Qifan Wang <qzw0023@auburn.edu>
2024-08-22 19:06:27 -0500

    add code to handle identifier and numbers

--- mic.asd
+++ mic.asd
@@ -1,3 +1,4 @@
 (asdf:defsystem "mic"
+  :depends-on ("re")
   :components ((:file "packages")
                (:file "tokenizer" :depends-on ("packages"))))
--- tokenizer.lisp
+++ tokenizer.lisp
@@ -43,9 +43,34 @@
                     #'read-identifier)
                    ((digitp char)
                     #'read-number)
+                   ((quotep char)
+                    #'read-string)
                    (t
                     (read-punctuactor)))
              state stream)))
 
+(defconstant +c-keywords+
+  (re:compile-re
+   (concatenate 'string
+                "auto|enum|restrict|unsigned|break|"
+                "extern|return|void|case|float|short|volatile|"
+                "char|for|signed|while|const|goto|sizeof|"
+                "_Bool|continue|if|static|_Complex|"
+                "default|inline|struct|_Imaginary|do|int|"
+                "switch|double|long|typedef|else|register|union")))
 
+;; Note that "0" is not a decimal number in ISO C99.
+(defconstant +c-decimal-integer+
+  (re:compile-re "[1-9]+[0-9]*(?[uUlL]|ll|LL)?"))
 
+(defun match-keywords (string)
+  "if the string matches a C keyword, return the keyword."
+  (re:match-string (re:match-re +c-keywords+ string :exact t)))
+
+(defun read-till-punctuactor (buffer stream)
+  "Handles both identifier or numbers."
+  )
+
+(defun read-identifier (state stream)
+  "Handles identifier or keywords"
+  )
