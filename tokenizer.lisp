320cecd09b83536fc170ca30f3a77685990d2609
Qifan Wang <qzw0023@auburn.edu>
2024-08-23 14:45:54 -0500

    partial implementation of read-punctuactor

--- tokenizer.lisp
+++ tokenizer.lisp
@@ -10,17 +10,17 @@
 
 ;; according to C99 lexical elements
 (defclass keyword (token)
-  ((kword :initarg :keyword :type symbol)))
+  ((kword :initarg :content :type symbol)))
 
 (defclass identifier (token)
-  ((string :initarg :id :type string)))
+  ((string :initarg :content :type string)))
 (defclass constant (token)
-  ((value :initarg :value :type string)
+  ((value :initarg :content :type string)
    (type :initarg :type :type symbol)))
 (defclass string-literal (token)
   ())
 (defclass punctuactor (token)
-  ())
+  ((punct :initarg :content :type symbol)))
 
 (defclass parse-state ()
   ((pos :initform 0 :type integer)
@@ -81,11 +81,14 @@
   "if the string matches a C keyword, return the keyword."
   (re:match-string (re:match-re +c-keywords+ string :exact t)))
 
+(defun punctuactorp (char)
+  (find c "[](){}.+-&*+-~!/%<>=^?:;|,#"))
+
 (defun read-till-punctuactor (buffer stream)
   "Handles both identifier or numbers."
   (loop never (let ((c (peek-char nil stream nil nil)))
                 (or (whitespacep c)
-                    (find c "[](){}.+-&*+-~!/%<>=^?:;|,#")))
+                    (punctuactorp c)))
         do (vector-push-extend (read-char stream) buffer)))
 
 (defun read-identifier (state stream)
@@ -96,23 +99,38 @@
     (read-till-punctuactor buffer stream)
     (or
      (let ((w (match +c-keywords+ buffer)))
-       (and w (make-instance 'keyword :keyword (intern w))))
+       (and w (make-instance 'keyword :content (intern w))))
      (let ((w (match +c-identifier+ buffer)))
-       (and w (make-instance 'identifier :string w)))
+       (and w (make-instance 'identifier :content w)))
      (error "~S is not a valid identifier or keyword." buffer))))
 
 (defun read-number (state stream)
   "Handles identifier or keywords"
   (let ((buffer (parse-buffer state)))
-    ;; clear buffer
     (setf (fill-pointer buffer) 0)
     (read-till-punctuactor buffer stream)
     (or
      (let ((w (match +c-decimal-integer+ buffer)))
-       (and w (make-instance 'constant :value w :type 'decimal)))
+       (and w (make-instance 'constant :content w :type 'decimal)))
      (let ((w (match +c-octal-integer+ buffer)))
-       (and w (make-instance 'constant :value w :type 'octal)))
+       (and w (make-instance 'constant :content w :type 'octal)))
      (error "~S is not a valid number." buffer))))
+
+(defconstant +c-1-2-punctuator+
+  (re:compile-re "[[](){}.]"))
+
+(defun read-punctuactor (state stream)
+  "Read punctuators."
+  (let ((buffer (parse-buffer state)))
+    (setf (fill-pointer buffer) 0)
+    (vector-push (read-char stream) buffer)
+    (let ((c (peek-char nil stream nil nil)))
+      (when (punctuactorp c)
+        (vector-push c buffer)))
+    (or
+     (and (= (length buffer) 1)
+          (make-instance 'punctuactor :content (intern buffer)))
+     #|other cases|#)))
 
 (defun tokenizer (stream)
   "Tokenize text from a input stream."
