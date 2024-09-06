68b31c5611bf4804a6c5a45c34e9f7f99a7085a7
Qifan Wang <qzw0023@auburn.edu>
2024-09-06 00:16:16 -0500

    test parsec

--- tokenizer-mpc.lisp
+++ tokenizer-mpc.lisp
@@ -1,1 +1,18 @@
 (in-package #:mic-pc)
+
+(defun ?string-chars ()
+  (?not (?test ('member '(#\" #\\)))))
+
+(defun ?escape-sequence-char ()
+  (?satisfies (lambda (c)
+                (find c "'\"?\\abfnrtv" :test #'char-equal))))
+
+(defun =c-string ()
+  (=destructure (_ s _)
+      (=list (?eq #\")
+             (=subseq (%some (%or (?string-chars)
+                                  (?seq (?eq #\\)
+                                        (?escape-sequence-char)))))
+             (?eq #\"))))
+
+(parse "\"abcds\\n\"" (=c-string))
