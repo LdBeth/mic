5788e7f5ebc02442056b9a95bf5ce9eaad639ca2
Qifan Wang <qzw0023@auburn.edu>
2024-12-13 15:44:09 -0600

    update examples

--- driver.lisp
+++ driver.lisp
@@ -84,6 +84,7 @@
                          (setf ast (mic-ast:pass-2 ast))
                          (setf ast (mic-ast:pass-3 ast))
                          (setf ast (mic-ast:pass-4 ast))
+                         (setf ast (mic-ast:pass-5 ast))
                          (mic-ast:code-gen ast))))
                  (user-quit 0)))))))
   (format *error-output* "No input file.~%")
